4e7f4c1c2226949219f2e0d922f32b0571a4cb85
gh4ever <leonedoro@naver.com>
2022-02-13 14:24:10 +0200

    size

--- src/App.js
+++ src/App.js
@@ -7,33 +7,34 @@ export default function App() {
     <div className="App">
       <div className="container">
         <Weather defaultCity="Tel Aviv" />
-
-        <small>
-          This project was coded by{' '}
-          <a
-            href="https://github.com/gh4ever"
-            rel="nonreferrer"
-            target="_blank"
-          >
-            Erika
-          </a>{' '}
-          and is{' '}
-          <a
-            href="https://github.com/gh4ever/weather-react-app"
-            rel="nonreferrer"
-            target="_blank"
-          >
-            open-sourced on GitHub
-          </a>{' '}
-          and{' '}
-          <a
-            href="https://priceless-tereshkova-1e0273.netlify.app"
-            rel="nonreferrer"
-            target="_blank"
-          >
-            hosted on Netlify
-          </a>
-        </small>
+        <footer>
+          <small>
+            This project was coded by{' '}
+            <a
+              href="https://github.com/gh4ever"
+              rel="nonreferrer"
+              target="_blank"
+            >
+              Erika
+            </a>{' '}
+            and is{' '}
+            <a
+              href="https://github.com/gh4ever/weather-react-app"
+              rel="nonreferrer"
+              target="_blank"
+            >
+              open-sourced on GitHub
+            </a>{' '}
+            and{' '}
+            <a
+              href="https://priceless-tereshkova-1e0273.netlify.app"
+              rel="nonreferrer"
+              target="_blank"
+            >
+              hosted on Netlify
+            </a>
+          </small>
+        </footer>
       </div>
     </div>
   )
